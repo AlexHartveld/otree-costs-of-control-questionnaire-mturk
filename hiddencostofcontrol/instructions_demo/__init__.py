57b873a3d5c9f9533e0eb243bf2eb1942656fd66
Alexander H <alex@lingophant.com>
2023-01-12 16:54:37 +0100

    added our demo as another experiment with a separate instructions app

---
+++ hiddencostofcontrol/instructions_demo/__init__.py
@@ -0,0 +1,42 @@
+from otree.api import *
+
+
+class C(BaseConstants):
+    NAME_IN_URL = 'instructions_demo'
+    PLAYERS_PER_GROUP = None
+    NUM_ROUNDS = 1
+    INSTRUCTIONS_TEMPLATE = 'instructions_demo/instructions.html'
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    instruction_q1 = models.StringField(
+        choices=['Yes', 'No'],
+        widget=widgets.RadioSelectHorizontal,
+        label='Player A can set a minimum amount for Player B.'
+    )
+    instruction_q2 = models.StringField(
+        choices=['Yes', 'No'],
+        widget=widgets.RadioSelectHorizontal,
+        label='Player A knows about the decision of Player B before Player A he has to decide.'
+    )
+    instruction_q3 = models.IntegerField(
+        choices=[10,13,15,25],
+        widget=widgets.RadioSelectHorizontal,
+        label='If Player B decided to set a minimum amount of 10 and Player A moved 3 sliders correctly, how many points does Player B have in the end?'
+    )
+
+# PAGES
+class Introduction(Page):
+    form_model = 'player'
+    form_fields = ['instruction_q1','instruction_q2','instruction_q3']
+
+
+page_sequence = [Introduction]
--- hiddencostofcontrol/settings.py
+++ hiddencostofcontrol/settings.py
@@ -19,6 +19,12 @@ SESSION_CONFIGS = [
         display_name="Experiment 3: Questionnaire",
         app_sequence=['survey', 'demographics', 'feedback', 'payment_info'], 
         num_demo_participants=1
+    ),
+    dict(
+        name='trust+slider',
+        display_name="Experiment 1+2: Demo",
+        app_sequence=['informed_consent','demographics','instructions_demo', 'trust', 'sliders','feedback','payment_info'], 
+        num_demo_participants=2
     )
 ]
 
@@ -31,12 +37,12 @@ SESSION_CONFIG_DEFAULTS = dict(
     real_world_currency_per_point=0.01, participation_fee=0.25, doc="",
     mturk_hit_settings=dict(
         keywords='bonus, study',
-        title='Study: Hidden costs of control',
+        title='University Study: Hidden costs of control (short questionnaire or game with bonus)',
         description='Our experiment lets participants play a short game with other participants, and/or answer a few question. We are interested in how people provide effort under different conditions.',
         frame_height=500,
         template='global/mturk_template.html',
         minutes_allotted_per_assignment=8,
-        expiration_hours=1, # 7 * 24,
+        expiration_hours=12, # 7 * 24,
         qualification_requirements=[
             {
             'QualificationTypeId': "36AGRGPO8VC7SA83E1O82NC4TWFCAY",
@@ -80,7 +86,10 @@ ADMIN_USERNAME = 'admin'
 ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
 
 DEMO_PAGE_INTRO_HTML = """
-Here are some oTree games.
+Here are three experiments that aim to capture the hidden costs of control. Agents get an initial endowment (like a salary), and the Principal's payoff depends on what the Agent sends back.
+The third experiment consists of five vignette stories, offered in a random order and with one out of four conditions per vignette (trust, human control, automated control, and monitoring).
+The installation instructions are on GitHub: https://github.com/AlexHartveld/otree-costs-of-control-questionnaire-mturk
+We also made this project to work with mTurk workers because we want to compare this population of automated control workers with the students from the original Falk & Kosfeld (2006) study. 
 """
 
 
